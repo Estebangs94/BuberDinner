ef667f9dade2488f48ffd5cd7fe61b6800db9a66
Esteban Garcia <garcia.esteban@belonghome.com>
2022-06-14 19:27:31 -0300

    Basic AuthService implementation. Configured DependencyInjection for Application & Infastructure projects

---
+++ BuberDinner.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using BuberDinner.Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuberDinner.Application
+{
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Registers application services
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+            return services;
+        }
+    }
+}
---
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,28 @@
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+
+namespace BuberDinner.Application.Services.Authentication
+{
+    internal class AuthenticationService : IAuthenticationService
+    {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
+        public AuthenticationResult Login(string email, string password)
+        {
+            return new AuthenticationResult(new Guid(), "name", "lastname", email, "token");
+        }
+
+        public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+        {
+            var userId = Guid.NewGuid();
+
+            var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+            return new AuthenticationResult(new Guid(), firstName, lastName, email, token);
+        }
+    }
+}
